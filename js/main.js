d762b599f9de75b02b61c2f46ef37569e059a1ad
Sean Xavier <seanxhallion@gmail.com>
2017-03-16 18:52:29 -0400

    fixing naming conventions

--- js/main.js
+++ js/main.js
@@ -10,59 +10,49 @@ var removeClutter = function(){
 	resultOne.style.display = 'none';
 }
 
-var classOne = document.getElementById('test-1');
+var classOne = document.getElementById('img-choice-1');
 	classOne.addEventListener('click', function(){
 	handleSelection(1);
 });
 
-var classTwo = document.getElementById('test-2');
+var classTwo = document.getElementById('img-choice-2');
 	classTwo.addEventListener('click', function(){
 	handleSelection(2);
 	});
 
-// var classTwo = document.getElementById('test-2');
-// 	classTwo.addEventListener('click', function(){
-// 	var resultTwo = document.getElementById('main-text');
-// 	resultTwo.innerHTML = 'This is the second story result!';
-// 	optionChosen = 2;
-// 	});
+var classThree = document.getElementById('img-choice-3');
+	classThree.addEventListener('click', function(){
+	handleSelection(3);
+	});
 
-// var classThree = document.getElementById('test-3')
-// 	classThree.addEventListener('click', function(){
-// 	var resultThree = document.getElementById('main-text');
-// 	resultThree.innerHTML = 'This is the third story result!';
-// 	optionChosen = 3;
-// 	});
-
-// var classFour = document.getElementById('test-4')
-// 	classFour.addEventListener('click', function(){
-// 	var resultFour = document.getElementById('main-text');
-// 	resultFour.innerHTML = 'This is the fourth story result!';
-// 	optionChosen = 4;
-// 	});
+var classFour = document.getElementById('img-choice-4');
+	classFour.addEventListener('click', function(){
+	handleSelection(4);
+	});
 
 	handleSelection = function(optionChosen){
 		if (optionChosen === 1){
 			removeClutter();
-			var showResultOne = document.getElementById('text-test-1');
+			var showResultOne = document.getElementById('result-one');
 			showResultOne.style.display = 'block';
+
 		} else if(optionChosen === 2){
 			removeClutter();
-			var showResultTwo = document.getElementById('text-test-2');
+			var showResultTwo = document.getElementById('result-two');
 			showResultTwo.style.display = 'block';
+
+		} else if(optionChosen === 3){
+			removeClutter();
+			var showResultThree = document.getElementById('result-three');
+			showResultThree.style.display = "block";
+
+		} else if(optionChosen === 4){
+			removeClutter();
+			var showResultFour = document.getElementById('result-four');
+			showResultFour.style.display = 'block';
+
 		};
 
 	};
-
-
-	
-
-	// } else if(){
-
-	// }else if(){
-
-	// } else {
-
-	// };
 
 }); // closes doc.ready
